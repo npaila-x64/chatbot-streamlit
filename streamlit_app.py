0a7aa16c7b208a835691c306f6bcb7673b4f2959
Nicolás Paila <nicolasandresp6@gmail.com>
2024-07-18 21:37:39 -0400

    add authentication gateway

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,14 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+st.title("💬 XpertIA Showcase")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+text_input_container = st.empty()
+app_password = text_input_container.text_input("Enter password", type="password")
+
+if app_password == st.secrets["app_password"]:
+    text_input_container.empty()
+    openai_api_key = st.secrets["openai_api_key"]
 
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
